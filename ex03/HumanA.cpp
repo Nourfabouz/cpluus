731704a4838afccc2ef269cee47f25aa9e4fa7a2
Nourfabouz <100221567+Nourfabouz@users.noreply.github.com>
2022-12-13 01:28:30 +0100

    Add files via upload

---
+++ ex03/HumanA.cpp
@@ -0,0 +1,21 @@
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon &weapon):_weapon(weapon)
+{
+    this->_name = name;
+    /*
+     *   std::cout << "HumanA constructor called" << std::endl;
+     */
+}
+
+HumanA::~HumanA()
+{
+    /*
+     *   std::cout << "HumanA destructor called" << std::endl;
+     */
+}
+
+void HumanA::attack(void)
+{
+    std::cout << this->_name << " attacks with his " << this->_weapon.getType() << std::endl;
+}
---
+++ ex03/HumanB.cpp
@@ -0,0 +1,28 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+{
+    this->_name =name;
+    _weapon = nullptr;
+    /*
+     *   std::cout << "HumanB constructor called" << std::endl;
+     */
+}
+
+HumanB::~HumanB()
+{
+    /*
+     *   std::cout << "HumanB constructor called" << std::endl;
+     */
+}
+
+void HumanB::attack(void)
+{
+    if (_weapon != nullptr)
+        std::cout << this->_name << " attacks with his " << _weapon->getType() << std::endl;
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+    this->_weapon = &weapon;
+}
